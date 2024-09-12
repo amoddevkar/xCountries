23e401fbe50bc31edcab40af25042b2452434520
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2024-09-13 01:15:19 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -11,14 +11,19 @@ function App() {
   useEffect(() => {
 
     const fetchData = async () => {
+      try {
+        const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+        if (!response.ok) {
+          throw new Error("error");
 
-      const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        console.log(jsonData)
+        setCountries(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-      const jsonData = await response.json();
-      console.log(jsonData)
-      setCountries(jsonData);
+
 
 
     };
