206430877b38437e723d2fe77ba305e89bb077d4
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2024-09-13 01:00:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import CountryBox from './components/CountryBox';
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jsonData = await response.json();
+      console.log(jsonData)
+      setCountries(jsonData);
+
+
+    };
+
+    fetchData();
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {countries.map((ele, idx) => <CountryBox name={ele.name} img={ele.flag} key={idx} />)}
     </div>
   );
 }
